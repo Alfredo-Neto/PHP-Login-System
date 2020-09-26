1347890f039179edac64c2b59e04b89de857cb3a
Alfredo-Neto <alfredoneto934@gmail.com>
2020-09-26 04:21:59 -0400

    added logout.php

--- painel.php
+++ painel.php
@@ -1,6 +1,8 @@
 <?php
-include('verifica_login.php'); 
+include('verifica_login.php');
 session_start();
 ?>
 
 <h2>Olá, <?php echo $_SESSION['usuario']; ?></h2>
+<h2><a href="logout.php">Sair</a>
+    <h2>
--- verifica_login.php
+++ verifica_login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
 if (!$_SESSION['usuario']) {
-    header('index.php');
+    header('Location: index.php');
     exit();
 }

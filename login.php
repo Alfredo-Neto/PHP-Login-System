62b123048237bf687748fc1898b577ebf1d39892
Alfredo-Neto <alfredoneto934@gmail.com>
2020-09-26 03:47:29 -0400

    first commit

---
+++ login.php
@@ -0,0 +1,19 @@
+<?php
+include('conexao.php');
+
+if (empty($_POST['usuario']) || empty($_POST['senha'])) {
+    header('Location: index.php');
+    exit();
+}
+
+$usuario = mysqli_real_escape_string($conexao, $_POST['usuario']);
+$senha = mysqli_real_escape_string($conexao, $_POST['senha']);
+
+$query = "select usuario_id, usuario from usuario where usuario = '{$usuario}' and senha = md5('{$senha}')";
+
+$result = mysqli_query($conexao, $query);
+
+$row = mysqli_num_rows($result);
+
+echo $row;
+exit;
